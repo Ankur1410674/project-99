b3a6689c5baa88806c2fe9e8adc13b47aafbf101
Ankur1410674 <69246028+Ankur1410674@users.noreply.github.com>
2021-07-08 19:33:35 +0530

    Add files via upload

---
+++ RemoveFiles.py
@@ -0,0 +1,12 @@
+import os
+import shutil
+path = input("enter the name of the directory to be sauted")
+os.path.exists(path) 
+for file in listoffiles:
+    name,ext = os.path.splitext(file)
+    ext = ext[1:]
+    if ext == " ":
+        continue
+    if ctime = os.stat(path).st_ctime
+        return ctime
+  
